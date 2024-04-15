3705fdeecc90339edb6078853f583061ddc88cff
antonio <a.campanozzi@studenti.uniba.it>
2024-04-15 16:57:16 +0200

    scritte le classi Sentence, Paragraph e Text

--- code/Text.py
+++ code/Text.py
@@ -2,9 +2,17 @@ from EventLog_Text_Parser.code.Paragraph import Paragraph
 
 
 class Text:
+    __paragraphSet: list
 
-    def add(self, paragraph:Paragraph):
+    def add(self, paragraph: Paragraph):
+        self.__paragraphSet.append(paragraph)
 
-    def store(self, filepath:str):
+    def store(self, filepath: str):
+        with open(filepath, 'w') as file:
+            file.write(self.__str__())
 
     def __str__(self):
+        textstr = ''
+        for paragraph in self.__paragraphSet:
+            textstr += f'{paragraph}\n'
+        return textstr
