b47ecd4e832107033e16f3ecb6b23ca024acfb34
antonio <a.campanozzi@studenti.uniba.it>
2024-04-22 19:02:58 +0200

    aggiornata la classe EventLogProcesser come concordato

--- code/EventLogProcesser.py
+++ code/EventLogProcesser.py
@@ -19,3 +19,22 @@ class EventLogProcesser:
             for e in trc:
                 e['time:timestamp'] = int(datetime.timestamp(e.get('time:timestamp')) - timestampOfFirstEvent)
 
+    @staticmethod
+    def sortEventLog(elog: EventLog, timestampLabel: str = 'time:timestamp'):
+        listofTrcaes: list = []
+        timeStampkey = lambda t: t[0][timestampLabel]
+        for i in elog:
+            listofTrcaes.append(i)
+        listofTrcaes.sort(key=timeStampkey)
+        sortedEventLog = EventLog(listofTrcaes)
+        return sortedEventLog
+
+    @staticmethod
+    def train_test_Split(evLog: EventLog, xesDestinationpath: str, originalXesName: str, trainPrcntg: float = 0.8):
+        train_test_EventLogs = pm4py.objects.log.util.split_train_test.split(evLog, trainPrcntg)
+        trainEventLog = train_test_EventLogs[0]
+        testEventLog = train_test_EventLogs[1]
+        # writing a new .xes with only train part
+        pm4py.write_xes(trainEventLog, f'{xesDestinationpath}\\TRAIN_{originalXesName}')
+        # writing a new .xes with only test part
+        pm4py.write_xes(testEventLog, f'{xesDestinationpath}\\TEST_{originalXesName}')
