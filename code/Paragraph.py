a8c9eadd72fefd8fb22e8f57d0bed6d3869eea10
antonio <a.campanozzi@studenti.uniba.it>
2024-04-22 17:00:42 +0200

    aggiunte modifiche concordate a Paragraph

--- code/Paragraph.py
+++ code/Paragraph.py
@@ -4,10 +4,13 @@ from Sentence import Sentence
 class Paragraph:
     __globalSentence: Sentence
     __localSentences: list
+    __trcClass: object
+    __prefixSet: list
 
     def __init__(self, globalSentence: Sentence):
         self.__globalSentence = globalSentence
         self.__localSentences = []
+        self.__prefixSet = []
 
     def getGlobalSentence(self):
         return self.__globalSentence
@@ -15,8 +18,20 @@ class Paragraph:
     def getLocalSentence(self, index: int):
         return self.__localSentences[index]
 
+    def getPrefixSet(self):
+        return self.__prefixSet
+
     def addLocalSentence(self, locSentence: Sentence):
         self.__localSentences.append(locSentence)
+
+    def toSubParagraphs(self, prfxLen: int = 1):
+        while prfxLen <= self.__localSentences.__len__():
+            subParagraph = Paragraph(self.__globalSentence)
+            for i in range(prfxLen):
+                subParagraph.addLocalSentence(self.__localSentences[i])
+            self.__prefixSet.append(subParagraph)
+            prfxLen += 1
+        return self.__prefixSet
 
     def __str__(self):
         paragraphstr = f'{self.getGlobalSentence()}\n'
